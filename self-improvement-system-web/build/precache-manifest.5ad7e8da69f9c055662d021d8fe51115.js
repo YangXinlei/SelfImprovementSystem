81507d4b0eddd203cfb2a7ec85a8e1baa69a9490
yangxinlei <yangxinlei@kuaishou.com>
2019-09-21 22:32:53 +0800

    add build

---
+++ self-improvement-system-web/build/precache-manifest.5ad7e8da69f9c055662d021d8fe51115.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464acb15a877889d5e42ff58b60e4d1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a4db55d3ffaec0c68a3",
+    "url": "/static/css/2.d2e72ad4.chunk.css"
+  },
+  {
+    "revision": "8c01688e59d11f8c3262",
+    "url": "/static/css/main.c033d734.chunk.css"
+  },
+  {
+    "revision": "3a4db55d3ffaec0c68a3",
+    "url": "/static/js/2.71870bd3.chunk.js"
+  },
+  {
+    "revision": "8c01688e59d11f8c3262",
+    "url": "/static/js/main.296c30f9.chunk.js"
+  },
+  {
+    "revision": "d1a6b0a6918992287fa6",
+    "url": "/static/js/runtime~main.0136eb7c.js"
+  }
+]);
